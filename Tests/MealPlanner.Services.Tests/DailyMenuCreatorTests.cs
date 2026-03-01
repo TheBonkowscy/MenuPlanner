00b1a3e13f8e16c857e7317fa362bfcdf2a0f090
Jan Bońkowski <devbonkowski@gmail.com>
2026-03-01 12:29:25 +0100

    Added basic project to store unit tests for services

---
+++ Tests/MealPlanner.Services.Tests/DailyMenuCreatorTests.cs
@@ -0,0 +1,13 @@
+﻿using MealPlanner.Services.DailyMenus.Create;
+
+namespace MealPlanner.Services.Tests;
+
+public class DailyMenuCreatorTests
+{
+    private DailyMenuCreator _sut = new();
+    
+    [Fact]
+    public void Test1()
+    {
+    }
+}
