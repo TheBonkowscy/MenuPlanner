3a45fda482cf1e4919c7ceae6ee941a151a99a15
Jan Bońkowski <devbonkowski@gmail.com>
2026-03-27 12:58:38 +0100

    Added PostgreSql integration

---
+++ MealPlanner.API/DatabaseOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace MealPlanner.API;
+
+public sealed class DatabaseOptions
+{
+    public const string SectionName = "Database";
+
+    public string Host { get; set; } = string.Empty;
+    public string Database { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public short Port { get; set; }
+}
--- MealPlanner.API/Program.cs
+++ MealPlanner.API/Program.cs
@@ -1,8 +1,11 @@
+using MealPlanner.API;
 using MealPlanner.API.Menus;
 using MealPlanner.Persistence;
 using MealPlanner.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<MealPlannerDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+builder.Services.AddOptions<DatabaseOptions>().Bind(builder.Configuration.GetSection(DatabaseOptions.SectionName));
+builder.Services.AddDbContext<MealPlannerDbContext>((services, ctx) =>
+{
+    var config = services.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+    var connectionStringBuilder = new NpgsqlConnectionStringBuilder()
+    {
+        Host = config.Host,
+        Database = config.Database,
+        Username = config.Username,
+        Password = config.Password,
+        Port = config.Port
+    };
+    ctx.UseNpgsql(connectionStringBuilder.ConnectionString);
+});
 
 builder.Services.AddHttpContextAccessor();
 
